208d8e3d3e581fc1cc1b184fd65226c7560d963f
Gr_n RF <gr.nrfinfo@gmail.com>
2022-03-24 10:55:40 +0600

    added Task10

---
+++ Seminar_3/Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов 
+             чисел от 1 до N.
+                                3 -> 1, 8, 27
+                                5 -> 1, 8, 27, 64, 125 */
+void Cub(double number)
+{
+    double num = 1;
+    double rezult;
+    while (num <= number)
+    {
+        rezult = Math.Pow(num,3);
+        Console.Write(rezult);
+        Console.Write(", ");
+        num = num + 1;
+    }
+}
+double x;
+Console.Write("Введите Ваше любимое положительно число: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write(x + " --> ");
+Cub(x);
