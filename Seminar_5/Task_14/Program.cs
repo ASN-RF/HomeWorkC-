82a8e804f48704a25a09ed41edb3d90e013e76a1
Gr_n RF <gr.nrfinfo@gmail.com>
2022-05-06 08:52:02 +0600

    added Task14

---
+++ Seminar_5/Task_14/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+   которая покажет количество чётных чисел в массиве.
+                                                    [345, 897, 568, 234] -> 2*/
+Console.WriteLine("Приветсвую! Помогите! Надо СРОЧНО задать размерность массива!!! Введите число!!! Жизнь программы в Ваших пальцах!");
+int razmer = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Все отлично справился! Решил задачу!!! Наверное!");
+int[] a = new int[razmer];
+int pol = 0;
+Console.Write("[");
+for (int i = 0; i < razmer; i++)
+{
+    a[i] = new Random().Next(100, 999);
+    if (i < razmer - 1) Console.Write(a[i] + " ");
+    if (i >= razmer - 1) Console.Write(a[i] + "]");
+}
+for (int i = 0; i < razmer; i++)
+{
+    if (a[i] % 2 == 0) pol++;
+}
+Console.Write(" --> " + pol);
+Console.WriteLine();
+Console.WriteLine("Количество чётных чисел в массиве с размерностью - " + razmer + " => " + pol);
