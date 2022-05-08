d8271c54eef10948ec43d81ee5a4ca7b3eddb7b6
Gr_n RF <gr.nrfinfo@gmail.com>
2022-05-08 12:39:35 +0600

    added Task22 correct

--- Seminar_8/Task_22/Program.cs
+++ Seminar_8/Task_22/Program.cs
@@ -6,14 +6,11 @@
                                 8 4 2 4                             2 4 4 8 */
 void RandomMatrix(int[,] array)
 {
-    // int[,] matrix = new int[m, n];
-
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i, j] = new Random().Next(1, 10);
-            // Console.Write($"{array[i, j]} ");
         }
     }
 }
