0cd6c604ac795f88667f280909871fd8031a84cb
Gr_n RF <gr.nrfinfo@gmail.com>
2022-06-04 11:51:39 +0600

    correct Task_19

--- Seminar_7/Task_19/Program.cs
+++ Seminar_7/Task_19/Program.cs
@@ -4,7 +4,7 @@
                                                                             1 -3,3 8 -9,9
                                                                             8 7,8 -7,1 9 */
 //1 вариант
-/*double[,] MatrixMxN(int m, int n)
+double[,] MatrixMxN(int m, int n)
 {
     double[,] matrix = new double[m, n];           //!вещественные числа
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -35,23 +35,28 @@ Console.Write("n = ");
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] matrix = MatrixMxN(m, n);
 PrintMatrixMxN(matrix);
-Console.WriteLine();*/
+Console.WriteLine();
 
 // 2 вариант
-void RandomMatrix(int m, int n)
-{
-    double[,] matrix = new double[m, n];
+// void RandomMatrix(int m, int n)
+// {
+//     double[,] matrix = new double[m, n];
 
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            // matrix[i, j] = new Random().Next(-99, 100);
-            matrix[i, j] = Math.Round(new Random().Next(-99, 100) + new Random().NextDouble(), 2);
-            Console.Write($"{matrix[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-RandomMatrix(3, 4);
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matrix[i, j] = Math.Round(new Random().Next(-99, 100) + new Random().NextDouble(), 2);
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Console.WriteLine("Введите количество строк:");
+// Console.Write("m = ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// Console.Write("n = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// RandomMatrix(m, n);
 
